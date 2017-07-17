80989c7b2a9647bd068d78aee81a5872e1030326
Chandler <cwm795@gmail.com>
2017-07-16 23:13:58 -0500

    I have the functions defined and 'if-else' statement set up. not finished, pushing so I can finish again tomorrow.

--- main.js
+++ main.js
@@ -1,28 +1,37 @@
 console.log("Hi");
-let input = document.getElementById('input');
-let button = document.getElementById('button');
-let container = document.getElementById('container');
+let input = document.getElementById("input");
+let button = document.getElementById("button");
+let container = document.getElementById("container");
 var testString = "";
 
 
+button.addEventListener("click", coolStuff )
 
+function (coolStuff) {
+	let newInput = input.value.split("");
+	let reverseInput = reversal(newInput);
+	container.innerHTML += alphabits(newInput);
+	container.innerHTML += palindrome(newInput, reverseInput);
+};
 
-
-
-
-
-
-function reversal() {
-
+function reversal(string) {
+	var reverse = string.reverse().join("");
+	container.innerHTML += `<p>${reverse}</p>`;
+	return reverse
 }
 
-function alphabits() {
+function alphabits(string) {
+	return string.sort().join("");;
+};
 
+function palindrome(string, reverseString) {
+	if (reverseString === input.value) {
+		return `<p>This is a palindrome</p>`
+	} else {
+		return `<p>This is not a palindrome</p>`
+	}
 }
 
-function palindrome() {
-
-}
 
 
 reversal(testString);
